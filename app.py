b9f4ac673b2f8772348e7d2b7883e6968ffad269
ChristineWasike <christinewasike22@gmail.com>
2018-12-08 16:56:50 +0200

    Initial Commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+#route
+@app.route('/')
+def ping():
+    return jsonify({
+        'status': 200,
+        'message': 'success',
+        'payload': 'ping'
+    })
+
+if __name__ == '__main__':
+    app.run(debug = True, host = '0.0.0.0')
