391d5c1180c6666734beb2a67dfc9c31e25b86d5
ChristineWasike <christinewasike22@gmail.com>
2018-12-08 18:10:31 +0200

    Made changes to the app and docker files

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
 from flask import Flask, jsonify
+import requests
+from requests.auth import HTTPBasicAuth
 
 app = Flask(__name__)
+
+#new
+class Config:
+    BASE_URL = 'http://178.128.165.87'
+
+class Send(Config):
+    def __init__(self, token, to, text, sender='MPESA'):
+        self.token = token
+        self.to = to
+        self.text = text
+        self.sender = sender
+        self.url = '{}/sms/'.format(Config.BASE_URL)
+    
+    def sms(self):
+        payload = {
+            'to': self.to,
+            'text': self.text,
+            'sender': self.sender
+        }
+
+        r = request.post{
+            self.url,
+            auth = HTTPBasicAuth(self.token, ''),
+            json = payload
+        }
+        return r.json()
 
 #route
 @app.route('/')
